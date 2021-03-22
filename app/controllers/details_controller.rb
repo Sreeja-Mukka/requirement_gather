204ece65d07ae5b5a881f8f1794b27124831cb99
Sreeja Mukka <mukkasreeja@gmail.com>
2021-03-22 17:26:31 +0530

    pages created

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,4 +1,5 @@
-class DetailsController < ActionController::Base
+class DetailsController < ApplicationController
+
     def index
         @details = Detail.all
     end
@@ -8,8 +9,24 @@ class DetailsController < ActionController::Base
     end
 
     def create
+        @detail = Detail.new(details_params)
+        respond_to do |format|
+            if @detail.save
+              format.html { redirect_to details_path, notice: 'Request successfully posted.' }
+              format.json { render :show, status: :created, location: @detail }
+            else
+              format.html { render :new }
+              format.json { render json: @detail.errors, status: :unprocessable_entity }
+            end
+          end
     end
     
     def show
+        @detail=Detail.find(params[:id])
+    end
+
+    private
+    def details_params
+        params.require(:detail).permit(:department_id ,:title ,:description ,:objective ,:analytics ,:expected_deadline)
     end
 end
